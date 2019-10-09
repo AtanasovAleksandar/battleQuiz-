26c544b6b5d059246e3d24b298e2ee28bfd022ba
Aleksandar Atanasov <aleksandaratanasov43@gmail.com>
2019-10-09 14:58:13 +0200

    git init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { HomePageComponent } from './quiz/home-page/home-page.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserProfileComponent },
+  { path: 'home' , component: HomePageComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { HomePageComponent } from './quiz/home-page/home-page.component';
+
+var config = {
+  apiKey: "AIzaSyA0PmLtYI_QwmHACD2RwCmsi-sjo2UeKGQ",
+  authDomain: "quiz-e446b.firebaseapp.com",
+  databaseURL: "https://quiz-e446b.firebaseio.com",
+  projectId: "quiz-e446b",
+  storageBucket: "",
+  messagingSenderId: "659699816728",
+  appId: "1:659699816728:web:b17bec89ad86fc9d0f666d"
+};
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
